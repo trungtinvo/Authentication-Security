a1c042bbae3db0c0cb095b86aab8fded3c17bf5b
trungtinvo <votrungtin0992@gmail.com>
2021-09-12 00:32:36 -0700

    Add bcrypt and salt for password

--- app.js
+++ app.js
@@ -3,15 +3,16 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 
-console.log(process.env.API_KEY);
-
 app.use(express.static("public"));
 app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 mongoose.connect("mongodb://localhost:27017/userDB");
 
@@ -39,12 +40,15 @@ app.get("/login", (req, res) => {
 
 app.post("/register", (req, res) => {
 
+  const hash = bcrypt.hashSync(req.body.password, saltRounds);
+
   const newUser = new User({
-      email: req.body.username,
-      password: md5(req.body.password)
+    email: req.body.username,
+    password: hash
   });
+
   newUser.save((err) => {
-    if(err) {
+    if (err) {
       console.log(err);
     } else {
       res.render("secrets");
@@ -53,22 +57,22 @@ app.post("/register", (req, res) => {
 });
 
 app.post("/login", (req, res) => {
-  const userPassword = md5(req.body.password);
+  const userPassword = req.body.password;
   const userEmail = req.body.username;
 
-  User.findOne({email: userEmail}, (err, foundUser) => {
-    if(err) {
+  User.findOne({
+    email: userEmail
+  }, (err, foundUser) => {
+    if (err) {
       console.log(err);
     } else {
       if (foundUser) {
-        if (foundUser.password === userPassword) {
-          res.render("secrets");
-        }
+        bcrypt.compareSync(userPassword, foundUser.password);
+        res.render("secrets");
       }
     }
   });
 });
-
 
 app.listen("3000", (req, res) => {
   console.log("Server started on port 3000.")
